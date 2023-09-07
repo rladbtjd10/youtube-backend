090a6948c3acfd81dbaaa337bc93a66ecee8dd0a
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-09-07 12:32:39 +0900

    feat:2023.09.07일 수업

--- src/main/java/com/kh/youtube/domain/Channel.java
+++ src/main/java/com/kh/youtube/domain/Channel.java
@@ -1,5 +1,7 @@
 package com.kh.youtube.domain;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,8 @@ public class Channel {
     private String channelDesc;
     private Date channelDate;
 
+    @ManyToOne // Channel 엔티티와 Member 엔티티를 다대일 관계로 설정
+    @JoinColumn(name="member_id") // 외래키 생성 or Member 엔티티의 기본키와 매핑
     private Member member;
 
 
